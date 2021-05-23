337cf78501311351f6add139ffd853a8522e5379
Paolo Cosentino <pcosentino95@gmail.com>
2021-05-23 03:39:19 -0700

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
         Graph g = new Graph();
         g.printAdjacencyList();
 
